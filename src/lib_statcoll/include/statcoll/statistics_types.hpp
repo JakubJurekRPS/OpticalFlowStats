fb2e80224b9334ff18336b3aebf0208a97abf2c8
Jakub Jurek <jakubjurek945@gmail.com>
2024-11-25 10:14:41 +0100

    Use copy and swap idiom for copy and move assignment operations

--- src/lib_statcoll/include/statcoll/statistics_types.hpp
+++ src/lib_statcoll/include/statcoll/statistics_types.hpp
@@ -14,54 +14,50 @@ using hsize_t = unsigned short;
 template <typename T = unsigned>
 class Histogram
 {
-    hsize_t checkSize(const hsize_t size) const
+    hsize_t checkSize(const int size) const
     {
-        if(size > std::numeric_limits<hsize_t>::max())
-            throw std::invalid_argument("Histogram size is too large.");
-        else if (size <= 1)
+        if (size <= 1)
+        {
             throw std::invalid_argument("Histogram size is too small. -> " 
                 + std::to_string(size) + " <= 1"); 
+        }
         return size;
     }
 public:
-    Histogram(const hsize_t binsNum): mBinsNum_{checkSize(binsNum)}, mData_{new T[binsNum]} 
+    explicit Histogram(const hsize_t binsNum): mBinsNum_{checkSize(binsNum)}, mData_{new T[binsNum]} 
     {
         std::fill(mData_, mData_ + binsNum, 0);
     }
     
+    //copy constructor
     Histogram(const Histogram & other) : mBinsNum_{other.mBinsNum_}, mData_{new T[mBinsNum_]}
     {
         std::copy(other.mData_, other.mData_ + mBinsNum_, mData_);
+        std::cout << "copy constructor" << std::endl;
     }
 
-    Histogram & operator=(const Histogram & other)
+    friend void swap(Histogram & first, Histogram & second) noexcept
     {
-        if(this == &other)
-            return *this;
-        delete[] mData_;
-        mBinsNum_ = other.mBinsNum_;
-        mData_ = new T[mBinsNum_];
-        std::copy(other.mData_, other.mData_ + mBinsNum_, mData_);
+        using std::swap;
+        swap(first.mBinsNum_, second.mBinsNum_);
+        swap(first.mData_, second.mData_);
+    }
+    //move constructor
+    Histogram(Histogram&& other) noexcept
+        : mBinsNum_(std::exchange(other.mBinsNum_, 0)),
+          mData_(std::exchange(other.mData_, nullptr)) { std::cout << "move constructor" << std::endl; }
+
+    //use copy swap idiom to implement copy and move assignment operators
+    Histogram & operator=(Histogram other)
+    {
+        // TODO: remove the comment below
+        std::cout << "move or copy assignment operator" << std::endl;
+        using std::swap;
+        swap(mBinsNum_, other.mBinsNum_);
+        swap(mData_, other.mData_);
         return *this;
     }
 
-    Histogram(Histogram && other) noexcept 
-    {
-        mBinsNum_ = other.mBinsNum_;
-        mData_ = other.mData_;
-        other.mData_ = nullptr;
-    }
-
-    Histogram & operator=(Histogram && other) noexcept
-    {
-        if(this == &other)
-            return *this;
-        delete[] mData_;
-        mBinsNum_ = other.mBinsNum_;
-        mData_ = other.mData_;
-        other.mData_ = nullptr;
-        return *this;
-    }
     auto begin() const 
     { 
         return mData_; 
@@ -90,7 +86,7 @@ public:
     }
     ~Histogram() { delete[] mData_; }
 private:
-    hsize_t mBinsNum_;
+    hsize_t mBinsNum_ = 0;
     T* mData_;
 };
 
